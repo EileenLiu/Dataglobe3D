5ffb1d5f5776b1ec8edd4556e09cdd9aa33ed81a
myuh <cg447@cornell.edu>
2015-09-19 13:52:11 -0400

    copy of files to test css

---
+++ geographico/js/visualize.js
@@ -0,0 +1,94 @@
+function buildDataVizGeometries( linearData ){	
+
+	var loadLayer = document.getElementById('loading');
+	loadLayer.style.display = 'none';	
+}
+
+function selectVisualization( linearData, year, countries, exportCategories, importCategories ){
+	//	we're only doing one country for now so...
+	var cName = countries[0].toUpperCase();
+	var affectedCountries = [cName];
+	$("#hudButtons .countryTextInput").val(cName);
+	previouslySelectedCountry = selectedCountry;
+	selectedCountry = countryData[countries[0].toUpperCase()];
+    
+	selectedCountry.summary = {
+		imported: {
+			mil: 0,
+			civ: 0,
+			ammo: 0,
+			total: 0,
+		},
+		exported: {
+			mil: 0,
+			civ: 0,
+			ammo: 0,
+			total: 0,
+		},
+		total: 0,
+		historical: getHistoricalData(selectedCountry),
+	};
+
+	// console.log(selectedCountry);
+
+	//	clear off the country's internally held color data we used from last highlight
+	for( var i in countryData ){
+		var country = countryData[i];
+		country.exportedAmount = 0;
+		country.importedAmount = 0;
+		country.mapColor = 0;
+	}
+
+	//	clear markers
+	for( var i in selectableCountries ){
+		removeMarkerFromCountry( selectableCountries[i] );
+	}
+
+	//	clear children
+	while( visualizationMesh.children.length > 0 ){
+		var c = visualizationMesh.children[0];
+		visualizationMesh.remove(c);
+	}
+
+	
+	for( var i in affectedCountries ){
+		var countryName = affectedCountries[i];
+		var country = countryData[cName];
+		attachMarkerToCountry( countryName, country.mapColor );
+	}
+
+	// console.log( mesh.affectedCountries );
+	highlightCountry( affectedCountries );
+
+	if( previouslySelectedCountry !== selectedCountry ){
+		if( selectedCountry ){
+			rotateTargetX = selectedCountry.lat * Math.PI/180;
+			var targetY0 = -(selectedCountry.lon - 9) * Math.PI / 180;
+            var piCounter = 0;
+			while(true) {
+                var targetY0Neg = targetY0 - Math.PI * 2 * piCounter;
+                var targetY0Pos = targetY0 + Math.PI * 2 * piCounter;
+                if(Math.abs(targetY0Neg - rotating.rotation.y) < Math.PI) {
+                    rotateTargetY = targetY0Neg;
+                    break;
+                } else if(Math.abs(targetY0Pos - rotating.rotation.y) < Math.PI) {
+                    rotateTargetY = targetY0Pos;
+                    break;
+                }
+                piCounter++;
+                rotateTargetY = wrap(targetY0, -Math.PI, Math.PI);
+			}
+            // console.log(rotateTargetY);
+            //lines commented below source of rotation error
+			//is there a more reliable way to ensure we don't rotate around the globe too much? 
+			/*
+			if( Math.abs(rotateTargetY - rotating.rotation.y) > Math.PI )
+				rotateTargetY += Math.PI;		
+			*/
+			rotateVX *= 0.6;
+			rotateVY *= 0.6;		
+		}	
+	}
+    
+    d3Graphs.initGraphs();
+}
