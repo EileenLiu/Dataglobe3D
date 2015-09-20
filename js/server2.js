3d7a1e593c715d2e42764aad8fa62215538a05db
EileenLiu <contact.eileenliu@gmail.com>
2015-09-20 04:24:00 -0400

    server2.js handles specific get requests for data of a given country

---
+++ js/server2.js
@@ -0,0 +1,22 @@
+var util = require("util"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+     
+server = http.createServer(function(request, response) {
+  var uri = url.parse(request.url).pathname;
+  if (uri == '/index.html/data' && request.method == 'GET') {
+      console.log("GET");
+      var query = url.parse(request.url, true).query;
+      var country = query.cn;
+      console.log("country=");
+      console.log(country);
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end();
+  }
+});
+port = 3000;
+host = '127.0.0.1'
+server.listen(port, host);
+console.log('Listening at http://' + host + ':' + port);
