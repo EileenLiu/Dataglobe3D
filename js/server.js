70ddb1e9301a46dd24d7eb66e109b2d821146a68
EileenLiu <contact.eileenliu@gmail.com>
2015-09-20 02:19:27 -0400

    node.js server that displays file if it exists, 404 if not

---
+++ js/server.js
@@ -0,0 +1,23 @@
+var util = require("util"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+     
+http.createServer(function(request, response) {
+    var uri = url.parse(request.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+    fs.readFile(filename, function (err, file) {
+       if (err) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+       }
+       response.writeHead(200);
+       response.write(file, "binary");
+       response.end();
+    });
+}).listen(8888);
+ 
+console.log("Server running at http://localhost:8888/");
